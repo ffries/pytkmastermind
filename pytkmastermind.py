fcac2b2ec8171f8c8116bb38e1ae8df1ac7b9fa2
Magali <magali.quertier@ac-versailles.fr>
2025-06-24 11:50:51 +0200

    Travail debut interface Magali

--- pytkmastermind.py
+++ pytkmastermind.py
@@ -1,8 +1,177 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 20 15:38:59 2025
+#Créé par quertier2, le 20/06/2025 en Python
+#Bibliotheques
+import tkinter as tk
+import numpy as np
 
-@author: mario
-"""
+
+#variables globales
+nbparties=1
+largeurfenetre=600
+hauteurfenetre=800
+listejoueur=["toto","tata"]
+decodeur=0
+codeur=1
+nbcouleurs=6
+dictcouleurs={-1:"light gray",-2:"gray64",0:"red",1:"green",2:"blue",3:"yellow",4:"black",5:"white"}
+nbcolonnes=6
+nblignes=10
+colonne=0
+ligne=0
+pasx=(largeurfenetre-20)/(2*nbcolonnes+1)
+pasy=((4*hauteurfenetre)/5)/(nblignes+1)
+matjeu=[[-1]*nbcolonnes for i in range(nblignes+1)]
+matreponse=[[-1]*nbcolonnes for i in range(nblignes)]
+
+
+couleurs=["gray64","black","white","red","green","blue","yellow","purple"]
+
+
+
+
+def cercle(canva,x,y,r,couleur):
+    """
+    Fonction qui dessine un cercle
+    Parameters
+    ----------
+    canva : Canvas
+    x : int abscisse du centre 
+    y : int ordonnee du centre
+    r : int rayon du centre
+    couleur : string correspondant à la couleur
+    Returns
+    -------
+    None.
+    """
+    canva.create_oval(x-r, y-r,x+r,y+r, fill=couleur)
+    
+def rectangle(canva,x,y,largeur,hauteur,couleur):
+    canva.create_rectangle(x-largeur/2,y-hauteur/2,x+largeur/2,y+hauteur/2,fill=couleur)
+
+
+def ligne_jeu(canva,listecercle,dictcouleurs,positionligne):
+    """
+    Fonction qui dessine une ligne de cercle dans l'espace jeu ou dans l'eapce réponse
+    Parameters
+    ----------
+    canva : Canva
+    listecercle : liste d'int correspondant au code couleur des cercles à dessiner
+    dictcouleurs : dictionnaire de string contenant les couleurs utilisées
+    positionligne : int correpondant à l'ordonnée de  la ligne à dessiner'
+    Returns
+    -------
+    None.
+    """
+    global largeurfenetre
+    global pasx
+    global nbcolonnes
+    for i in range(nbcolonnes):
+        couleur=dictcouleurs[listecercle[i]]
+        cercle(canva,pasx*(i+1),positionligne,pasx/4,couleur)
+        
+def ligne_reponse(canva,listecercle,dictcouleurs,positionligne):
+    global largeurfenetre
+    global pasx
+    global nbcolonnes
+    for i in range(nbcolonnes):
+        couleur=dictcouleurs[listecercle[i]]
+        rectangle(canva,pasx*(i+1),positionligne,pasx/4,pasy/4,couleur)
+    
+def bouton_clique():
+    print("coucou") 
+
+def jouer():
+    global hauteurfenetre
+    global nblignes
+    coup=[1,2,4,3,5,6,2]
+    ligne_jeu(cnv,coup, dictcouleurs, ((4*hauteurfenetre)/5)/((nblignes+1)))
+
+def initialisation_plateau():
+    #Ne prend pas de paramètre car sinon on ne peut pas l'appeler avec un bouton
+    """ 
+    Fonction qui initialise le plateau de jeu en créant les ronds vides en fonction du nombre de lignes
+    et de colonnes définis
+    """
+    global paxy
+    global nbcolonnes
+    global dictcouleurs
+    global pasy
+    initligne=[-2]*nbcolonnes
+    for i in range(nblignes):
+        ligne_jeu(cnv,initligne,dictcouleurs,pasy*(1+i))
+        ligne_reponse(cnv2,initligne,dictcouleurs,pasy*(1+i))
+
+def choisircouleur(matrice,numerocouleur):
+    global ligne
+    global colonne
+    global dictcouleurs
+    print("couleur bouton:",dictcouleurs[numerocouleur])
+    cercle(cnv,pasx*(colonne+1),pasy*(ligne+1),pasx/4,dictcouleurs[numerocouleur])
+    #mise à jour de la matrice à faire
+    if(colonne<nbcolonnes):
+        colonne+=1
+    print (colonne)
+    return None
+
+# Création de la fenêtre principale
+root = tk.Tk()
+root.title("Mastermind")
+#chaine de caractère pour definir la surface de la fenetre en focntion de la largeur et de la hauteur définies
+surface=str(largeurfenetre)+"x"+str(hauteurfenetre)
+root.geometry(surface)
+#Création des Label frame poour séparer l'espace de jeu du panneau de commande
+espacecommande = tk.LabelFrame(root,text="Espace commande",relief='groove')
+espacecommande.pack(side="bottom",padx=10,pady=10,fill="x")
+espacejeu = tk.LabelFrame(root,text="Espace jeu",relief='groove')
+espacejeu.pack(side="left",fill="y", expand=1)
+espacereponse= tk.LabelFrame(root,text="Espace reponse",relief='groove')
+espacereponse.pack(side="right",fill="y",expand=1)
+
+
+#dessin de l'espace jeu
+cnv=tk.Canvas(espacejeu, height=0.8*hauteurfenetre, bg=dictcouleurs[-1])
+cnv.pack(padx=0, pady=0, ipady=largeurfenetre/2)
+
+#dessin de l'espace réponse
+cnv2=tk.Canvas(espacereponse, height=0.8*hauteurfenetre, bg=dictcouleurs[-1])
+cnv2.pack(padx=0, pady=0, ipady=largeurfenetre/4)
+
+
+# Espace commande
+buttoninit=tk.Button(espacecommande,text="Commencer", command=initialisation_plateau)
+buttoninit.pack(side="top", fill="x")
+button = tk.Button(espacecommande,text="Cliquez ici", command=jouer)
+button.pack(side="bottom")
+buttonquit=tk.Button(espacecommande, text = 'Quitter', command = root.destroy)
+buttonquit.pack(side="bottom")
+#Initialisation de la liste qui va contenir les boutons de couleurs
+boutons_couleur=[0]*nbcouleurs 
+#Création du bouton annuler qui correspond à la couleur d'initialisation couleurs[0]
+boutons_annuler=tk.Button(espacecommande,text="Annuler", command=lambda : cercle(cnv,20,20,10,"red") )
+boutons_annuler.pack(side="left", fill="x", expand=1)
+#Parcours de la liste de couleurs pour créer les boutons de couleurs sauf la couleur d'initialisation
+for i in range(nbcouleurs):
+    print("dictionnaire de couleurs",dictcouleurs[i], "i=", i)
+    #i=i permet de garder la valeur actuelle de i sinon on capteur la référence à la variable i qui continue d'évoluer et vaut 5 quand on clique sur le bouton
+    boutons_couleur[i]=tk.Button(espacecommande,text=dictcouleurs[i],command=lambda i=i:choisircouleur(matjeu,np.int64(i)))
+    boutons_couleur[i].pack(side="left", fill="x", expand=1)
+# boutons_couleur[0]=tk.Button(espacecommande,text=dictcouleurs[0],command=lambda:choisircouleur(matjeu,0))
+# boutons_couleur[0].pack(side="left", fill="x", expand=1)
+# boutons_couleur[1]=tk.Button(espacecommande,text=dictcouleurs[1],command=lambda:choisircouleur(matjeu,1))
+# boutons_couleur[1].pack(side="left", fill="x", expand=1)
+
+#Boucle principale
+
+
+#initialisation_plateau()
+positionligne=40
+# coul=(couleurs[2])
+# #cercle(cnv,20,20,10,"red")
+
+# coup=[0,1,2,4,5,6,0]
+# ligne_jeu(cnv,coup, couleurs, 80)
+# res=[7,6,5,7,7]
+# ligne_jeu(cnv2,res, couleurs, 40)
+
+
+root.mainloop()
 
